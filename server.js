ef3eae14f649079754f517119d29f040beaa5b7a
Richard Ramos <richardarturo@gmail.com>
2023-02-18 17:00:17 -0400

    First

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express ()
+
+app.set("view engine", "ejs")
+
+app.get("/", (req,res) => {
+    res.render("index")
+})
+
+app.post("/upload", (req,res) =>{
+    res.send("hi")
+})
+app.listen(3000, ()=> {
+    console.log("listening")
+}
+) 
