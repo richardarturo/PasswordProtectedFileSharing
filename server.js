f4650b20ded811a65f83da5b77193daf8237bfb0
Richard Ramos <richardarturo@gmail.com>
2023-02-23 13:23:44 -0400

    PPFS

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
+require("dotenv").config()
+const multer = require("multer")
+const mongoose = require ("mongoose")
+const bcrypt = require("bcrypt")
+const File = require("./models/File")
+
 const express = require("express")
 const app = express ()
+
+const upload = multer ({ dest: "uploads" })
+
+mongoose.connect(process.env.DATABASE_URL)
 
 app.set("view engine", "ejs")
 
@@ -7,10 +17,29 @@ app.get("/", (req,res) => {
     res.render("index")
 })
 
-app.post("/upload", (req,res) =>{
-    res.send("hi")
+app.post("/upload", upload.single("file"), async (req,res) => {
+   const fileData ={
+    path: req.file.path,
+    originalName: req.file.originalname
+   }
+   if (req.body.password != null && req.body.password !=="") {
+    fileData.password = bcrypt.hash(req.body.password, 10)
+   }
+
+   const file = await File.create(fileData)
+
+   res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}`})
 })
-app.listen(3000, ()=> {
-    console.log("listening")
-}
-) 
+
+
+app.get("/file/:id", async (req, res) => {
+    const file = await File.findById(req.params.id)
+
+    file.downloadCount++
+    await file.save()
+    console.log(file.downloadCount)
+
+    res.download(file.path, file.originalName)
+})
+
+app.listen(process.env.PORT) 
